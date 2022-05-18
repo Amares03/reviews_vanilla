bc1b09e8d47eca737293d2680c2ace06e694289f
Amares03 <amaressabit0314@gmail.com>
2022-05-18 16:20:02 +0300

    set info

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ const reviews = [
     id: 1,
     name: "Amir Siraj",
     job: "web developer",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
     id: 2,
     name: "Alemu Fikadu",
     job: "web designer",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
     text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
   },
   {
     id: 3,
     name: "Mensur Muhammed Kemal",
     job: "intern",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
     text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
   {
@@ -40,12 +40,25 @@ const prevBtn = document.getElementById("prev-btn");
 const nextBtn = document.getElementById("next-btn");
 const randomBtn = document.getElementById("random-btn");
 
-let currentItem = 0;
-// load inital
-window.addEventListener("DOMContentLoaded", function () {
-  const item = reviews[currentItem];
+// show person function
+function showPerson(person) {
+  const item = reviews[person];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
+}
+let currentItem = 0;
+// load inital
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson(currentItem);
+});
+
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  showPerson(currentItem);
+});
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  showPerson(currentItem);
 });
